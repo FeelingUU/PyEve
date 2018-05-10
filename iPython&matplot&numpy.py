3266b24664fcb929713a51c418f9fe181c1bb30d
andy.qu <andy.qu@9an-data.net>
2018-05-10 18:29:44 +0800

    20180510

--- iPython&matplot&numpy.py
+++ iPython&matplot&numpy.py
@@ -39,9 +39,31 @@ if __name__ == '__main__':
     # print(c.shape)
 
 
-    a = np.arange(12).reshape(1,1,1,12)
-    print(a)
-    print("---------")
-    print(np.squeeze(a))
+    # a = np.arange(12).reshape(1,3,4)
+    # b = np.arange(13,25).reshape(1,3,4)
+    # print(a)
+    # print(b)
+    # print("---------")
+    # print(np.stack((a,b),0))
+    # print(np.stack((a,b),0).shape)
+    # print(np.hstack((a,b)))
+    # print(np.vstack((a,b)))
+
+    # a = np.arange(12).reshape(3,4)
+    # print(a.ndim)
+    # print(a)
+    # print(np.resize(a,(5,13)))
+    # print(np.append(a,[[1,1,1,1],[1,1,1,1],[1,1,1,1]],axis=1))
+    # print(np.insert(a,0,[3,4,32],axis=1))
+    # print(np.stack((a,a)))
+    # print(np.delete(a,2,axis=1))
+    # print(np.unique(np.stack((a,a)),return_counts=True))
+
+    # print(np.invert(-13))
+    # print(np.invert(242))
+    print(np.left_shift(15,2))
+
+    print(np.char.add("aaa"," aaa"))
+    print(np.char.add(["aaa"," aaa"],['dsd ',u'都是']))
 
     pass
